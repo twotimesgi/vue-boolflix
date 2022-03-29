31f1a02d6ac0894142b9eace1c0982ad90be7e59
twotimesgi <luigibardellagerbi@mail.com>
2022-03-29 10:41:40 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-comp />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/HeaderComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComp
   }
 }
 </script>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <img class="logo" src="../assets/logo.png">
+    <div class="filters"></div>
+  </header>
+</template>
+
+<script>
+export default {
+   name: 'HeaderComp', 
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+header{
+  height: 80px;
+  width: :;
+}
+</style>
